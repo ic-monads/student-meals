fa797880ce64e689381108612a17f0f3b8933ea9
Euan Turner <euanoturner@gmail.com>
2023-02-08 13:40:14 +0000

    Super simple recipetile example

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
+import RecipeTile from "./components/recipes/RecipeTile";
+import mashedPotatoes from "./interfaces/examples/Recipe";
+import { BrowserRouter as Router } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Student Meals
-        </p>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Student Meals</h1>
+          <RecipeTile recipe={mashedPotatoes}/>
+        </header>
+      </div>
+    </Router>
   );
 }
 
--- src/components/recipes/RecipeTile.tsx
+++ src/components/recipes/RecipeTile.tsx
@@ -1,1 +1,39 @@
-export {}
+import {Recipe} from "../../interfaces/recipes/Recipe";
+import React from "react";
+import { Link } from "react-router-dom";
+
+// const DifficultyRatingComponent = (difficulty : number) => {
+//   return (
+//     <div className="difficultyRating">
+//       {[...Array(5)].map((star) => {
+//         return (
+//           <span className={"star"} id={star}>&#9733</span>
+//         );
+//       })
+//       }
+//     </div>
+//   )
+// }
+
+interface RecipeTileProps {
+  recipe : Recipe;
+}
+
+function RecipeTile(props : RecipeTileProps) {
+  const { recipe } = props;
+  return (
+    <div className="recipe-tile">
+      <img src={recipe.images[0]} alt={recipe.name}/>
+      <section className="recipe-info">
+        <Link to={`/recipes/${recipe.id}`}>
+          <h2 className="recipe-name">{recipe.name}</h2>
+        </Link>
+        <p>Time: {recipe.time}</p>
+        <p>Difficulty: {recipe.difficulty}</p>
+        <p>Cuisine : {recipe.cuisine}</p>
+      </section>
+    </div>
+  );
+}
+
+export default RecipeTile;
