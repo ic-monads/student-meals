cd0edd0c2fb827127bc69a6d1a7da10ac5040807
Daniel Wilcox <danielwilcox@pc-206-114.customer.ask4.lan>
2023-02-09 21:18:51 +0000

    Established recipes list and page

---
+++ src/interfaces/examples/ExampleRecipeList.ts
@@ -0,0 +1,3 @@
+import mashedPotatoes from "./Recipe";
+
+export default Array(30).fill(mashedPotatoes);
--- src/pages/recipes/RecipePage.tsx
+++ src/pages/recipes/RecipePage.tsx
@@ -1,1 +1,28 @@
-export {}
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { recipes } from "../../interfaces/examples/Recipe";
+import { Recipe } from "../../interfaces/recipes/Recipe";
+import Ingredients from "../../components/recipes/Ingredients";
+import Equipment from "../../components/recipes/Equipment";
+import Instructions from "../../components/recipes/Instructions";
+
+export default function RecipePage() {
+  const { recipeId } = useParams();
+  const [recipe, setRecipe] = useState<Recipe>();
+
+  useEffect(() => {
+    setRecipe(recipes.find(r => r.id == recipeId));
+  }, [])
+
+  return(
+    <div>
+      <h1>{recipe?.name}</h1>
+      <p>Difficulty: {recipe?.difficulty}</p>
+      <p>Cuisine: {recipe?.cuisine}</p>
+      <p></p>
+      <Ingredients ingredients={recipe?.ingredients!} />
+      <Equipment equipment={recipe?.equipment!} />
+      <Instructions instructions={recipe?.instructions!} />
+    </div>
+  );
+}
