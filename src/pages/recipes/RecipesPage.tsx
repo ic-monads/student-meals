e821dda8b821634460d5d019a48208a6510d3913
Euan Turner <euanoturner@gmail.com>
2023-02-10 09:33:50 +0000

    Simple styling of recipelist, page and tile

--- src/pages/recipes/RecipesPage.tsx
+++ src/pages/recipes/RecipesPage.tsx
@@ -5,7 +5,12 @@ import ExampleRecipeList from "../../interfaces/examples/ExampleRecipeList";
 function RecipesPage() {
     return (
         <>
-            <h1>Recipes</h1>
+          <div className="bg-amber-50 pb-10">
+            <h1 className="font-sans text-7xl text-lg font-semibold text-center">
+              Recipes
+            </h1>
+          </div>
+            
             <RecipeList recipes={ExampleRecipeList}/>
         </>
     )
