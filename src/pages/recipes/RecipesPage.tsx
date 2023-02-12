2d1b93cb55742d13f82910451b7b4828a84a837e
Daniel Wilcox <danielwilcox@Daniels-MacBook-Pro.local>
2023-02-12 17:28:06 +0000

    Added More button for recipeslist page

--- src/pages/recipes/RecipesPage.tsx
+++ src/pages/recipes/RecipesPage.tsx
@@ -1,12 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import RecipeList from "../../components/recipes/RecipeList";
 import ExampleRecipeList from "../../interfaces/examples/ExampleRecipeList";
+import { Recipe } from "../../interfaces/recipes/Recipe";
 
 function RecipesPage() {
+    const defaultNumRecipes = 10;
+    const [recipes, setRecipes] = useState<Recipe[]>([]);
+    const [numRecipes, setNumRecipes] = useState(defaultNumRecipes);
+    const [loading, setLoading] = useState(false);
+
+    const handleMoreClick = () => {
+        setNumRecipes((numRecipes) => numRecipes + 10);
+    }
+
+    useEffect(() => {
+        async function loadRecipes() {
+            setLoading(true);
+            try {
+                const data = ExampleRecipeList.slice(0, numRecipes);
+                if(numRecipes === defaultNumRecipes) {
+                    setRecipes(data);
+                } else {
+                    setRecipes((recipes) => [...recipes, ...data]);
+                }
+            } catch (e) {
+                console.log(e);
+            } finally {
+                setLoading(false);
+            }
+        }
+        loadRecipes();
+    }, [numRecipes]);
+
     return (
         <>
             <h1>Recipes</h1>
-            <RecipeList recipes={ExampleRecipeList}/>
+            {loading && (
+                <div className="center-page">
+                <span className="spinner primary"></span>
+                <p>
+                    Loading...
+                </p>
+                </div>
+            )}
+            <RecipeList recipes={recipes}/>
+            <div className="row">
+                <div className="col-sm-12">
+                    <div className="button-group fluid">
+                        <button className="button default" onClick={handleMoreClick}>
+                            More
+                        </button>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
